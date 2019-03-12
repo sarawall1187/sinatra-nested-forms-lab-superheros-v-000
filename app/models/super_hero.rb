b13f26cecd647dead2d014c6f788e48936f05d7e
Sara Walker <sarawall1187@gmail.com>
2019-03-12 20:27:59 +0000

    Automatically backed up by Learn

--- app/models/super_hero.rb
+++ app/models/super_hero.rb
@@ -1,15 +1,15 @@
-class SuperHero 
+class SuperHero
   attr_accessor :name, :power, :bio
   @@all = []
   def intitialize(name, power, bio)
     @name = name
     @power = power
-    @bio = bio 
-    @@all << self 
+    @bio = bio
+    @@all << self
   end
 
-  def self.all 
-    @@all 
+  def self.all
+    @@all
   end
 
 end
