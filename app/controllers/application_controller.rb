bf369cf4226cb08aa6c2f6b4ba70d7929a369fe0
Sara Walker <sarawall1187@gmail.com>
2019-03-12 20:46:24 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class App < Sinatra::Base
     end
 
     post '/teams' do
-      binding.pry
+  
       @team = Team.new(params[:team][:name], params[:team][:motto])
 
       params[:team][:heroes].each do |details|
